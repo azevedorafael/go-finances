02db509b6e842dd127cedea3d803170d79ff1bd4
azevedorafael <you@example.com>
2020-04-21 23:06:36 -0300

    Fix outcome value

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -90,7 +90,11 @@ const Dashboard: React.FC = () => {
                 transactions.map(transaction => (
                   <tr key={transaction.id}>
                     <td className="title">{transaction.title}</td>
-                    <td className="income">{formatValue(transaction.value)}</td>
+                    <td className="income">
+                      {`${
+                        transaction.type === 'outcome' ? '-' : ''
+                      } ${formatValue(transaction.value)} `}
+                    </td>
                     <td>
                       {transaction.category && transaction.category.title}
                     </td>
